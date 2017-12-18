5bd38e79c16e90c1b523126d59a7e49a73127db2
C3NZ <epichdd@gmail.com>
2017-12-17 23:31:53 -0500

    Changed the name of the server socket

--- server.py
+++ server.py
@@ -1,18 +1,21 @@
 import socket
 
 if __name__ == '__main__':
-	sock = socket.socket()
+	server = socket.socket()
 	print("Created socket successfully")
 
 	port = 1111
 
-	sock.bind(('', port))
+	server.bind(('', port))
 	print('Socket binded to:', port)
 
-	sock.listen(5)
+	server.listen(5)
 	listening = True
 
 	while listening:
-		connection, addr = sock.accept()
+		connection, addr = server.accept()
+		threading.Thread(target=)
 		connection.send(b"Hey, what's up!")
 		connection.close()
+
+	server.close()
